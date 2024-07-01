6a6c043d4750c25b81909030c94ad91140b8b569
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-07-02 00:51:55 +0200

    Fix root

--- scripts/start_invokeai.sh
+++ scripts/start_invokeai.sh
@@ -3,7 +3,7 @@ export PYTHONUNBUFFERED=1
 echo "Starting InvokeAI"
 cd /workspace/InvokeAI
 source /venv/bin/activate
-nohup invokeai-web > /workspace/logs/invokeai.log 2>&1 &
+nohup invokeai-web --root /workspace/InvokeAI > /workspace/logs/invokeai.log 2>&1 &
 echo "InvokeAI started"
 echo "Log file: /workspace/logs/invokeai.log"
 deactivate
