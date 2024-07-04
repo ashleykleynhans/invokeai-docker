3b6c740ea8a0160703fe33627f7f041cdb7ddda8
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2024-07-04 08:37:53 +0200

    Fix venv path

--- app-manager/start_invokeai.sh
+++ app-manager/start_invokeai.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export PYTHONUNBUFFERED=1
 cd /workspace/InvokeAI
-source /venvs/invokeai/bin/activate
+source /venv/bin/activate
 nohup invokeai-web --root /workspace/InvokeAI > /workspace/logs/invokeai.log 2>&1 &
